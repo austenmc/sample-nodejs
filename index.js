6637e0bf2965e558d880a0d4ba7571b9cfbd0489
Austen McDonald <austen.mcdonald@gmail.com>
2024-10-22 10:38:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ var lorem = new LoremIpsum({
   }
 });
 
-app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)))
+app.get('/', (req, res) => res.send(lorem.generateParagraphs(process.env.JOC_PARAGRAPHS)))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}! paragraphs: ${process.env.JOC_PARAGRAPHS}`))
